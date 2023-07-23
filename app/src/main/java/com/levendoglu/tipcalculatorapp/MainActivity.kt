8a3a0d43540656216b94ed29ea05054de99a4008
Ayberk Levendoğlu <ayberklevendoglu@gmail.com>
2023-07-23 21:51:49 +0300

    Initial commit

---
+++ app/src/main/java/com/levendoglu/tipcalculatorapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.levendoglu.tipcalculatorapp
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.levendoglu.tipcalculatorapp.databinding.ActivityMainBinding
+import kotlin.math.roundToInt
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonCalculate.setOnClickListener {
+            calculateTip()
+        }
+    }
+    @SuppressLint("SetTextI18n")
+    fun calculateTip(){
+        val serviceCost = binding.serviceCost.text.toString().toDouble()
+        val amazing = binding.radioButtonAmazing
+        val good = binding.radioButtonGood
+        val okay = binding.radioButtonOkay
+        val switch = binding.switch1
+        if (amazing.isChecked && switch.isChecked) binding.textViewTipAmount.text = "Tip amount: $${((serviceCost * 20) / 100).roundToInt()}"
+        if (amazing.isChecked && !switch.isChecked) binding.textViewTipAmount.text = "Tip amount: $${((serviceCost * 20) / 100)}"
+        if (good.isChecked && switch.isChecked) binding.textViewTipAmount.text = "Tip amount: $${((serviceCost * 18) / 100).roundToInt()}"
+        if (good.isChecked && !switch.isChecked) binding.textViewTipAmount.text = "Tip amount: $${((serviceCost * 18) / 100)}"
+        if (okay.isChecked && switch.isChecked) binding.textViewTipAmount.text = "Tip amount: $${((serviceCost * 15) / 100).roundToInt()}"
+        if (okay.isChecked && !switch.isChecked) binding.textViewTipAmount.text = "Tip amount: $${((serviceCost * 15) / 100)}"
+    }
+}
